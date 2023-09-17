4dd4c01487498aebe4fef23771c775299617c3bb
thatAwesomeKK <32800130+thatAwesomeKK@users.noreply.github.com>
2023-09-17 17:05:41 +0530

    vercel config

---
+++ src/app.js
@@ -0,0 +1,36 @@
+import "dotenv/config.js";
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import passport from "passport";
+
+const app = express();
+
+import authRouter from "./routes/auth.js";
+import serviceRequestRouter from "./routes/serviceRequest.js";
+import profileRouter from "./routes/profile.js";
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+app.use(helmet());
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(morgan("dev"));
+app.use(express.json({ limit: "1000mb" }));
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use("/api/auth", authRouter);
+app.use("/api/service-request", serviceRequestRouter);
+app.use("/api/profile", profileRouter);
+
+export default app
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import app from "./app.js";
+import { connectToMongo } from "./config/db.js";
+
+const PORT = process.env.PORT || 5000;
+
+//serves the application at the defined port if Connected to MongoDB
+connectToMongo()
+  .then(() => {
+    try {
+      app.listen(PORT, () => {
+        console.log("Connected to MongoDB");
+        console.log(`Server is running on : http://localhost:${PORT}`);
+      });
+    } catch (error) {
+      console.log("Cannot Connect to Server");
+    }
+  })
+  .catch((e) => {
+    console.log("Error In Connecting to Server!");
+  });
