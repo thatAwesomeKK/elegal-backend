47206efcf0c6471a5d4b2d717ca8c45eee9a7f31
thatAwesomeKK <32800130+thatAwesomeKK@users.noreply.github.com>
2023-09-23 18:39:32 +0530

    env example

--- src/middleware/verifySession.js
+++ src/middleware/verifySession.js
@@ -3,7 +3,7 @@ import User from "../models/User.js";
 export default async function (req, res, next) {
   try {
     const {user: userId} = req.session
-    console.log(userId);
+    
     if (!userId) {
       return res.status(401).json({ success: false, error: "Not Authorized" });
     }
