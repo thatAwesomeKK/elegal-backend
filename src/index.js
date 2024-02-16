50d07a3f3378eef2a9aa65ae8c3ba8fcaa8336a1
thatAwesomeKK <2550492.kk@gmail.com>
2024-02-16 20:24:51 +0530

    vercel push

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import app from "./app.js";
 import { connectToMongo } from "./config/db.js";
 
-
 const PORT = process.env.PORT || 5000;
 
 //serves the application at the defined port if Connected to MongoDB
-connectToMongo()
-  .then(() => {
-    try {
-      app.listen(PORT, () => {
+app.listen(PORT, () => {
+  connectToMongo()
+    .then(() => {
+      try {
         console.log("Connected to MongoDB");
-        console.log(`Server is running on : http://localhost:${PORT}`);
-      });
-    } catch (error) {
-      console.log("Cannot Connect to Server");
-    }
-  })
-  .catch((e) => {
-    console.log("Error In Connecting to Server!");
-  });
+      } catch (error) {
+        console.log("Cannot Connect to Server");
+      }
+    })
+    .catch((e) => {
+      console.log("Error In Connecting to Server!");
+    });
+  console.log(`Server is running on : http://localhost:${PORT}`);
+});
