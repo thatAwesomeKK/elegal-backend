540d206a69d4a66e7d5ae97e1e039b7beea04ca3
thatAwesomeKK <2550492.kk@gmail.com>
2024-02-16 21:04:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import { connectToMongo } from "./config/db.js";
 const PORT = process.env.PORT || 5000;
 
 //serves the application at the defined port if Connected to MongoDB
-app.listen(PORT, () => {
-  connectToMongo()
-    .then(() => {
-      try {
-        console.log("Connected to MongoDB");
-      } catch (error) {
-        console.log("Cannot Connect to Server");
-      }
-    })
-    .catch((e) => {
-      console.log("Error In Connecting to Server!");
-    });
-  console.log(`Server is running on : http://localhost:${PORT}`);
-});
+connectToMongo()
+  .then(() => {
+    try {
+      console.log("Connected to MongoDB");
+      app.listen(PORT, () => {
+        console.log(`Server is running on : http://localhost:${PORT}`);
+      });
+    } catch (error) {
+      console.log("Cannot Connect to Server");
+    }
+  })
+  .catch((e) => {
+    console.log("Error In Connecting to Server!");
+  });
